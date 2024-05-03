84a8dc719233fd583ed96800349dbed70f6a316c
OMBRENORMAL <134549221+samibumbar@users.noreply.github.com>
2024-05-03 23:57:58 +0200

    ad new script page

--- js/modal.js
+++ js/modal.js
@@ -15,27 +15,3 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
-document.addEventListener('DOMContentLoaded', function() {
-  var navLinks = document.querySelectorAll('[data-active]');
-  function markActive() {
-    setTimeout((function(){
-      var currentUrl = window.location.href;
-  
-      navLinks.forEach(function(link) {
-        if(currentUrl.endsWith(link.getAttribute('data-active')) ){
-         link.classList.add('active')
-    
-        }
-        else{
-        link.classList.remove('active')
-        }
-      })
-    }))
-   
-  }
- markActive()
- navLinks.forEach(function(link)
-{
-  link.addEventListener("click",markActive)
-})
-});
